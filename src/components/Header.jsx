56f72ce9ca72150594268e603007d62bc76c9fb0
RohanScripts <shelarrohan1810@gmail.com>
2024-12-15 01:08:06 +0530

    Add Navbar and Header components with layout adjustments

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="min-h-screen bg-cover bg-center overflow-hidden w-full flex items-center"></div>
+  );
+};
+
+export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import logo from "../assets/logo.png";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="top-0 left-0 z-10">
+      <div className="flex justify-between items-center container mx-auto bg-transparent">
+        <img src={logo} className="cursor-pointer w-32" alt="" srcSet="" />
+        <ul className="hidden md:flex gap-5">
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Home
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            About
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Projects
+          </a>
+          <a href="#Header" className="cursor-pointer hover:text-gray-400">
+            Testimonials
+          </a>
+        </ul>
+        <button className="hidden md:block bg-white px-8 py-2 rounded-full">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
