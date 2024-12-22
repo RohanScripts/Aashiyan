4a43032e202e9a507ce8bd12c7a98bab54a5f699
RohanScripts <shelarrohan1810@gmail.com>
2024-12-22 13:29:08 +0530

    Refactor Testimonials component to include asset imports and enhance styling; add star rating display for testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,4 @@
-import { testimonialsData } from "../assets/assets";
+import { assets, testimonialsData } from "../assets/assets";
 const Testimonials = () => {
   return (
     <div className=" bg-white w-full overflow-hidden pb-10">
@@ -13,8 +13,22 @@ const Testimonials = () => {
       </p>
       <div className="flex justify-center gap-8 flex-wrap">
         {testimonialsData.map((eachTestimonial, index) => (
-          <div key={index}>
-            <img src={eachTestimonial.image} alt={eachTestimonial.alt} />
+          <div
+            key={index}
+            className="border shadow-lg rounded px-8 py-12 text-center"
+          >
+            <img
+              className="rounded-full mx-auto mb-5"
+              src={eachTestimonial.image}
+              alt={eachTestimonial.alt}
+            />
+            <h2>{eachTestimonial.name}</h2>
+            <p>{eachTestimonial.title}</p>
+            <div>
+              {Array.from({ length: eachTestimonial.rating }, (item, index) => (
+                <img key={index} src={assets.star_icon} alt="" />
+              ))}
+            </div>
           </div>
         ))}
       </div>
