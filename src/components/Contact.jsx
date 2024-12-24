6c5f7776bc81ed7c8baba23cb710347f3d855bc7
RohanScripts <shelarrohan1810@gmail.com>
2024-12-24 23:04:29 +0530

    Add form to Contact component for user input; include fields for name and email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,6 +10,30 @@ const Contact = () => {
       <p className="text-center text-sm text-gray-700">
         Ready to Make a Move? Let's Build Your Future Together..
       </p>
+      <form>
+        <div>
+          <div className="w-full md:w-1/2 text-left">
+            <label htmlFor="">Your Name</label>
+            <input
+              type="text"
+              name="Name"
+              placeholder="Enter Your Name Here..."
+              required
+              className="border border-gray-300 rounded py-3 px-4 mt-2 w-full"
+            />
+          </div>
+          <div className="w-full md:w-1/2 text-left">
+            <label htmlFor="">Your Email</label>
+            <input
+              type="email"
+              name="Email"
+              placeholder="Enter Your Email..."
+              required
+              className="border border-gray-300 rounded py-3 px-4 w-full mt-2"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
