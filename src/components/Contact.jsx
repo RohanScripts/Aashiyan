e9ebb20451b72a233dfd5cb7d5774f09469e8acf
RohanScripts <shelarrohan1810@gmail.com>
2024-12-26 14:36:55 +0530

    Enhance Contact component with additional message field and submit button; improve form layout and styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -10,8 +10,8 @@ const Contact = () => {
       <p className="text-center text-sm text-gray-700">
         Ready to Make a Move? Let's Build Your Future Together..
       </p>
-      <form>
-        <div>
+      <form className="max-w-2xl mx-auto text-gray-600 pt-8">
+        <div className="flex flex-wrap ">
           <div className="w-full md:w-1/2 text-left">
             <label htmlFor="">Your Name</label>
             <input
@@ -19,19 +19,33 @@ const Contact = () => {
               name="Name"
               placeholder="Enter Your Name Here..."
               required
-              className="border border-gray-300 rounded py-3 px-4 mt-2 w-full"
+              className="border border-gray-300 rounded py-3 px-4 mt-1 w-full"
             />
           </div>
-          <div className="w-full md:w-1/2 text-left">
+          <div className="w-full md:w-1/2 text-left md:pl-4">
             <label htmlFor="">Your Email</label>
             <input
               type="email"
               name="Email"
               placeholder="Enter Your Email..."
               required
-              className="border border-gray-300 rounded py-3 px-4 w-full mt-2"
+              className="border border-gray-300 rounded py-3 px-4 w-full mt-1"
             />
           </div>
+        </div>
+        <div className="my-5 text-left">
+          <label htmlFor="">Message</label>
+          <textarea
+            name="Message"
+            required
+            placeholder="Enter Your Message Here..."
+            className="border border-gray-300 w-full resize-none rounded mt-1 h-48 px-4 py-2"
+          ></textarea>
+        </div>
+        <div className="">
+          <button className="bg-gradient-to-r from-pink-500 to-violet-500 px-4 py-2 text-white  rounded">
+            Submit
+          </button>
         </div>
       </form>
     </div>
